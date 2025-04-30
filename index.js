0472e117058524ab258d5b1558e714ad74d82676
unknown <braiantucker@gmail.com>
2025-04-30 07:00:39 -0300

    elementos eliminados y cree una lista con las cosas aprendidas

--- index.js
+++ index.js
@@ -25,6 +25,33 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  document.querySelector(".lista").innerHTML = "";
+
+  const terminal = document.createElement("li");
+  terminal.textContent = "Terminal";       
+  document.querySelector(".lista").appendChild(terminal); 
+
+  const node = document.createElement("li");
+  node.textContent = "Node";       
+  document.querySelector(".lista").appendChild(node);
+
+  const oop = document.createElement("li");
+  oop.textContent = "Oop";       
+  document.querySelector(".lista").appendChild(oop);
+
+  const typescript = document.createElement("li");
+  typescript.textContent = "Typescript";       
+  document.querySelector(".lista").appendChild(typescript); 
+
+  const css = document.createElement("li");
+  css.textContent = "Css";       
+  document.querySelector(".lista").appendChild(css);
+
+  const dom = document.createElement("li");
+  dom.textContent = "Dom";       
+  document.querySelector(".lista").appendChild(dom);
+  
+}
 
 main();
